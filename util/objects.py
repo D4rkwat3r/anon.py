614108ffede3758043a7622cad4c100f0c2f2d0c
D4rkwat3r <90906169+D4rkwat3r@users.noreply.github.com>
2021-12-01 22:32:32 +0300

    Update objects.py

--- util/objects.py
+++ util/objects.py
@@ -7,3 +7,18 @@ class UserProfileList:
             self.uid.append(user["id"])
             self.nickname.append(user["name"])
             self.login.append(user["login"])
+
+
+class PostList:
+    def __init__(self, responseData: list):
+        self.id = []
+        self.viewCount = []
+        self.text = []
+        self.authorName = []
+        self.authorId = []
+        for post in responseData:
+            self.id.append(post["id"])
+            self.viewCount.append(post["postviews"]["count"])
+            self.text.append(post["text"])
+            self.authorName.append(post["owner_name"])
+            self.authorId.append(post["owner_id"])
